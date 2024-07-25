61ff2844398dc4ea4cd67caf95b1256e5a68f3ae
Lúcia Helena <luciahproenca@gmail.com>
2024-07-24 23:08:47 -0300

    Convertendo o css

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { ContainerHero, FormHero, HeroTitle } from './style'
+
+const Hero = () => (
+  <FormHero>
+    <ContainerHero>
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </ContainerHero>
+  </FormHero>
+)
+
+export default Hero
